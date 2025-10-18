473d1c545f9cfd3cbd9c6280a19d5f9ee989f466
Caio Saboia <caiosaboia@profectum.com.br>
2025-10-18 00:14:13 -0300

    att copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,7 +1,9 @@
 return {
   "github/copilot.vim",
-  vim.keymap.set("i", "<C-l>", 'copilot#Accept("\\<CR>")', {
+  vim.keymap.set("i", "<C-l>", 'copilot#Accept("<CR>")', {
     expr = true,
     replace_keycodes = false,
   }),
+  vim.keymap.set("n", "<leader> ce", "<cmd>Copilot enable<CR>", { desc = "Enable Copilot" }),
+  vim.keymap.set("n", "<leader> cd", "<cmd>Copilot disable<CR>", { desc = "Disable Copilot" }),
 }
